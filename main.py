1301a122798ee70c664118acd4bf088ecc339837
Monika Szydłak <mnkkuc3@gmial.com>
2024-09-24 15:12:44 +0200

    Add main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+sasiedzi_dict = {
+    "Ukraina": "Kijów",
+    "Niemcy": "Berlin",
+    "Czechy": "Praga",
+    "Słowacja": "Bratysława",
+    "Białoruś": "Mińsk",
+    "Litwa": "Wilno",
+    "Rosja": "Moskwa"
+}
+
+# Iterowanie po kluczach i wartościach
+for kraj, stolica in sasiedzi_dict.items():
+    print(f"Kraj: {kraj}, Stolica: {stolica}")
+
